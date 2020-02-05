1a7760396ad15f1c09ad677a3ae50fb9e0802c43
0fajarpurnama0 <fajar.purnama@yahoo.com>
2020-02-06 00:37:56 +0900

    add contents

--- cryptocurrency/js/menu.js
+++ cryptocurrency/js/menu.js
@@ -1,42 +1,29 @@
 $(document).ready(function(){
   $(".navmenu").html(`
-          <li class="dropdown nav-menu"><a href="index.html" class="dropbtn">Home</a></li>
+         <li class="dropdown nav-menu">
+	    <a href=".." class="dropbtn">Home</a></li>
           <li class="dropdown nav-menu">
-            <a href="simple-mining.html" class="dropbtn">Simple Mining</a>
+            <a href="about.html" class="dropbtn">About</a>
             <div class="dropdown-content">
-			  <a id="monerowebminer" href="#">Monero Web Miner</a>			  
-              <a id="cryptotab" href="#">Cryptotab</a>
-			  <a id="gshare" href="#">GShare</a>
-			  <a id="vertcoinoneclickminer" href="#">Vertcoin One Click Miner</a>
+			  <a href="best-personal-advice-to-start.html">Best Personal Advice To Start</a>
+			  <a href="how-to-get-crypto">How To Get Crypto</a>			  
             </div>
           </li>
           <li class="dropdown nav-menu">
-            <a href="javascript:void(0)" class="dropbtn">Advanced Mining</a>
+            <a href="mining.html" class="dropbtn">Mining</a>
             <div class="dropdown-content">
-			  <a id="coin" href="#">Coin</a>
-              <a id="hardware" href="#">Hardware</a>
-			  <a id="pool" href="#">Pool</a>
-			  <a id="Software" href="#">Software</a>
+			  <a href="simple-mining.html">Simple Mining</a>
+			  <a href="regular-mining.html">Regular Mining</a>
+			  <a href="advance-mining.html">Advance Mining</a>			  
             </div>
           </li>
-          <li class="dropdown nav-menu">
-            <a href="javascript:void(0)" class="dropbtn">Monetized Mining</a>
-            <div class="dropdown-content">
-			  <a id="personalwebsite" href="#">Personal Website</a>
-              <a id="webmining" href="#">Web Mining</a>
-			  <a id="experimentground" href="#">Experiment Ground</a>
-            </div>
           </li>
           <li class="dropdown nav-menu">
-            <a href="javascript:void(0)" class="dropbtn">Withdrawing</a>
+            <a href="applications.html" class="dropbtn">Applications</a>
             <div class="dropdown-content">
-              <a href="#">Wallet</a>
-            </div>
-          </li>
-          <li class="dropdown nav-menu">
-            <a href="javascript:void(0)" class="dropbtn">Trading</a>
-            <div class="dropdown-content">
-              <a href="#"></a>
+			  <a href="browsers-that-rewards.html">Browser That Rewards</a>
+			  <a href="decentralized-platforms.html">Decentralized Platforms</a>
+			  <a href="crypto-faucets.html">Crypto Faucets</a>
             </div>
           </li>
    `);
