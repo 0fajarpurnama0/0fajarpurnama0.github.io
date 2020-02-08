3ee3d13a82c57ed40efccb03b2fd972257aede3b
0fajarpurnama0 <fajar.purnama@yahoo.com>
2020-02-08 19:45:54 +0900

    add tmp contents

---
+++ tmp/js/menu.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".navmenu").html(`
+			<li>
+				<a href=".." id="home">Home</a>
+			</li>
+			<li>
+				<a href="script.html">Script</a>
+			</li>
+			<li>
+				<a href="draft.html">Draft</a>
+			</li>
+   `);
+});
