e7787cfa688db470ac2db1e0e0703c4e2d3aefc5
0fajarpurnama0 <fajar.purnama@yahoo.com>
2020-02-20 02:05:16 +0900

    personal layout

--- cryptocurrency/js/menu.js
+++ cryptocurrency/js/menu.js
@@ -1,30 +1,25 @@
 $(document).ready(function(){
   $(".navmenu").html(`
-         <li class="dropdown nav-menu">
-	    <a href=".." class="dropbtn">Home</a></li>
-          <li class="dropdown nav-menu">
-            <a href="about.html" class="dropbtn">About</a>
-            <div class="dropdown-content">
-			  <a href="best-personal-advice-to-start.html">Best Personal Advice To Start</a>
-			  <a href="how-to-get-crypto">How To Get Crypto</a>			  
-            </div>
-          </li>
-          <li class="dropdown nav-menu">
-            <a href="mining.html" class="dropbtn">Mining</a>
-            <div class="dropdown-content">
-			  <a href="simple-mining.html">Simple Mining</a>
-			  <a href="regular-mining.html">Regular Mining</a>
-			  <a href="advance-mining.html">Advance Mining</a>			  
-            </div>
-          </li>
-          </li>
-          <li class="dropdown nav-menu">
-            <a href="applications.html" class="dropbtn">Applications</a>
-            <div class="dropdown-content">
-			  <a href="browsers-that-rewards.html">Browser That Rewards</a>
-			  <a href="decentralized-platforms.html">Decentralized Platforms</a>
-			  <a href="crypto-faucets.html">Crypto Faucets</a>
-            </div>
-          </li>
+			<li class="navmenu dropdown-1"><a href="..">Home</a></li>
+			<li class="navmenu dropdown-1"><a href="about.html">About</a>
+				<ul class="navmenu dropcontent-1">
+					<li class="navmenu dropdown-2"><a href="best-personal-advice-to-start.html">Best Personal Advice To Start</a></li>
+					<li class="navmenu dropdown-2"><a href="how-to-get-crypto">How To Get Crypto</a></li>
+				</ul>
+			</li>
+			<li class="navmenu dropdown-1"><a href="mining.html" class="dropbtn">Mining</a>
+				<ul class="navmenu dropcontent-1">
+				  <li class="navmenu dropdown-2"><a href="simple-mining.html">Simple Mining</a></li>
+				  <li class="navmenu dropdown-2"><a href="regular-mining.html">Regular Mining</a></li>
+				  <li class="navmenu dropdown-2"><a href="advance-mining.html">Advance Mining</a></li>			  
+				</ul>
+			</li>
+			<li class="navmenu dropdown-1"><a href="applications.html" class="dropbtn">Applications</a>
+				<ul class="navmenu dropcontent-1">
+				  <li class="navmenu dropdown-2"><a href="browsers-that-rewards.html">Browser That Rewards</a></li>
+				  <li class="navmenu dropdown-2"><a href="decentralized-platforms.html">Decentralized Platforms</a></li>
+				  <li class="navmenu dropdown-2"><a href="crypto-faucets.html">Crypto Faucets</a></li>
+				</ul>
+			</li>
    `);
 });
